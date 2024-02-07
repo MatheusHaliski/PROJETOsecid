2b4c9a54df2cc76e123f18ca61588e383db4ecf2
MatheusHaliski <matheushaliski@gmail.com>
2024-02-07 13:50:52 -0300

    listagem SECIDPR
    listagem SECIDPR

--- LISTAGEM/connection2.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbServerName = 'localhost';
-    $dbUserName = 'root';
-    $dbPassword = '';
-    $dbName = 'Regionais';
-
-$connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
-$sqe = "SELECT * FROM Regionais";
-?>
--- LISTAGEM/connection3.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbServerName = 'localhost';
-    $dbUserName = 'root';
-    $dbPassword = '';
-    $dbName = 'Regionais';
-
-$connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
-?>
